6eca5567eaaa222e08e2272491c378a0f0f8cbad
Christian <xtianus@live.com>
2019-05-31 00:45:40 -0400

    first push

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild Microsoft.Bot.Sample.QnABot.sln -p:DeployOnBuild=true -p:PublishProfile=learn-factbot-Web-Deploy.pubxml -p:Password=n0MHBhawsRNbx5CWivpWSPF1LbByyexx3i3tuZxtN9Hl1fwBHCxddAoFuGiu
+
