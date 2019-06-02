d5f8eba09c74503d85cbf4eb93a16b2a9c0860cc
Christian <xtianus@live.com>
2019-06-02 19:36:12 -0400

    update appjs

--- app.js
+++ app.js
@@ -1,90 +1,48 @@
-/*-----------------------------------------------------------------------------
-A simple echo bot for the Microsoft Bot Framework. 
------------------------------------------------------------------------------*/
+"use strict";
+var builder = require("botbuilder");
+var botbuilder_azure = require("botbuilder-azure");
+
+var useEmulator = true;
+var userName = "";
+var yearsCoding = "";
+var selectedLanguage = "";
+
+var connector = useEmulator ? new builder.ChatConnector() : new botbuilder_azure.BotServiceConnector({
+    appId: process.env.MicrosoftAppId,
+    appPassword: process.env.MicrosoftAppPassword
+});
+
+var bot = new builder.UniversalBot(connector);
+
+bot.dialog('/', [
+
+function (session) {
+    builder.Prompts.text(session, "Hello, and welcome to QnA Factbot! What's your name?");
+},
+
+function (session, results) {
+    userName = results.response;
+    builder.Prompts.number(session, "Hi " + userName + ", how many years have you been writing code?");
+},
+
+function (session, results) {
+    yearsCoding = results.response;
+    builder.Prompts.choice(session, "What language do you love the most?", ["C#", "Python", "Node.js", "Visual FoxPro"]);
+},
+
+function (session, results) {
+    selectedLanguage = results.response.entity;
+
+    session.send("Okay, " + userName + ", I think I've got it:" +
+        " You've been writing code for " + yearsCoding + " years," +
+        " and prefer to use " + selectedLanguage + ".");
+}]);
 
 var restify = require('restify');
-var builder = require('botbuilder');
-var botbuilder_azure = require("botbuilder-azure");
-var builder_cognitiveservices = require("botbuilder-cognitiveservices");
-
-// Setup Restify Server
 var server = restify.createServer();
-server.listen(process.env.port || process.env.PORT || 3978, function () {
-   console.log('%s listening to %s', server.name, server.url); 
-});
-  
-// Create chat connector for communicating with the Bot Framework Service
-var connector = new builder.ChatConnector({
-    appId: process.env.MicrosoftAppId,
-    appPassword: process.env.MicrosoftAppPassword,
-    openIdMetadata: process.env.BotOpenIdMetadata 
+
+server.listen(3978, function() {
+    console.log('test bot endpoint at http://localhost:3978/api/messages');
 });
 
-// Listen for messages from users 
 server.post('/api/messages', connector.listen());
-
-/*----------------------------------------------------------------------------------------
-* Bot Storage: This is a great spot to register the private state storage for your bot. 
-* We provide adapters for Azure Table, CosmosDb, SQL Azure, or you can implement your own!
-* For samples and documentation, see: https://github.com/Microsoft/BotBuilder-Azure
-* ---------------------------------------------------------------------------------------- */
-
-var tableName = 'botdata';
-var azureTableClient = new botbuilder_azure.AzureTableClient(tableName, process.env['AzureWebJobsStorage']);
-var tableStorage = new botbuilder_azure.AzureBotStorage({ gzipData: false }, azureTableClient);
-
-// Create your bot with a function to receive messages from the user
-var bot = new builder.UniversalBot(connector);
-bot.set('storage', tableStorage);
-
-// Recognizer and and Dialog for preview QnAMaker service
-var previewRecognizer = new builder_cognitiveservices.QnAMakerRecognizer({
-    knowledgeBaseId: process.env.QnAKnowledgebaseId,
-    authKey: process.env.QnAAuthKey || process.env.QnASubscriptionKey
-});
-
-var basicQnAMakerPreviewDialog = new builder_cognitiveservices.QnAMakerDialog({
-    recognizers: [previewRecognizer],
-    defaultMessage: 'No match! Try changing the query terms!',
-    qnaThreshold: 0.3
-}
-);
-
-bot.dialog('basicQnAMakerPreviewDialog', basicQnAMakerPreviewDialog);
-
-// Recognizer and and Dialog for GA QnAMaker service
-var recognizer = new builder_cognitiveservices.QnAMakerRecognizer({
-    knowledgeBaseId: process.env.QnAKnowledgebaseId,
-    authKey: process.env.QnAAuthKey || process.env.QnASubscriptionKey, // Backward compatibility with QnAMaker (Preview)
-    endpointHostName: process.env.QnAEndpointHostName
-});
-
-var basicQnAMakerDialog = new builder_cognitiveservices.QnAMakerDialog({
-    recognizers: [recognizer],
-    defaultMessage: 'No match! Try changing the query terms!',
-    qnaThreshold: 0.3
-}
-);
-
-bot.dialog('basicQnAMakerDialog', basicQnAMakerDialog);
-
-bot.dialog('/', //basicQnAMakerDialog);
-    [
-        function (session) {
-            var qnaKnowledgebaseId = process.env.QnAKnowledgebaseId;
-            var qnaAuthKey = process.env.QnAAuthKey || process.env.QnASubscriptionKey;
-            var endpointHostName = process.env.QnAEndpointHostName;
-
-            // QnA Subscription Key and KnowledgeBase Id null verification
-            if ((qnaAuthKey == null || qnaAuthKey == '') || (qnaKnowledgebaseId == null || qnaKnowledgebaseId == ''))
-                session.send('Please set QnAKnowledgebaseId, QnAAuthKey and QnAEndpointHostName (if applicable) in App Settings. Learn how to get them at https://aka.ms/qnaabssetup.');
-            else {
-                if (endpointHostName == null || endpointHostName == '')
-                    // Replace with Preview QnAMakerDialog service
-                    session.replaceDialog('basicQnAMakerPreviewDialog');
-                else
-                    // Replace with GA QnAMakerDialog service
-                    session.replaceDialog('basicQnAMakerDialog');
-            }
-        }
-    ]);
