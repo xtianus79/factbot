f7d11c5457c2799ac0b608ed203461df0323ae5b
Christian <xtianus@live.com>
2019-06-02 18:40:52 -0400

    updating

--- publish.cmd
+++
@@ -1,3 +0,0 @@
-nuget restore
-msbuild Microsoft.Bot.Sample.QnABot.sln -p:DeployOnBuild=true -p:PublishProfile=learn-factbot-Web-Deploy.pubxml -p:Password=n0MHBhawsRNbx5CWivpWSPF1LbByyexx3i3tuZxtN9Hl1fwBHCxddAoFuGiu
-
